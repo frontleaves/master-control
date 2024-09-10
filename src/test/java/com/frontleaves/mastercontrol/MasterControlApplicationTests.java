03544cd5c6ed6cf8b0311bc0dbfeae7c02eae2a5
XiaoLFeng <gm@x-lf.cn>
2024-09-10 08:30:06 +0800

    feat(init): first commit

---
+++ src/test/java/com/frontleaves/mastercontrol/MasterControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontleaves.mastercontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
